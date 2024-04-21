c94919cf77c367c609823debe42916f5206b1971
Zail <rekcahet@gmail.com>
2024-04-20 23:35:15 -0500

    terminada la pagina 404, redireccionando a Home

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,15 +1,23 @@
+import {Link} from "react-router-dom"
 
-import errorSvg from "../assets/404.svg"
+
 import "../styles/error.css"
 
 export default function Error() {
+
+  
   return (
     <>
-    <div className="container">
-      
-    <img src={errorSvg} alt="Error SVG" className='error-404' />
-    <h1>Error</h1>
-    </div>
+    <div id="notfound">
+  <div className="notfound">
+    <div className="notfound-404" />
+    <h1>404</h1>
+    <h2>Oops! Page Not Be Found</h2>
+    <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
+    <Link to="/">Back to homepage</Link>
+  </div>
+</div>
+
     
     </>
     
