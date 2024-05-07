2b8b2ef22eb74aac18a6699f19cfdd9070a220d7
FranMolleda <55050534+FranMolleda@users.noreply.github.com>
2024-05-07 13:13:34 +0200

    Revert "Fran"

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,3 +8,15 @@ module.exports = {
       "^.+\\.[t|j]sx?$": "babel-jest",
     },
   };
+// babel.config.js
+
+module.exports = {
+  setupFilesAfterEnv: ["./tests/components/setupTests.js"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+  },
+};
